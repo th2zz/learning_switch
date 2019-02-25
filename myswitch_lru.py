eaa74f3b2005658d6517d946e1f36d80ed60a99a
th2zz <tzhang329@wisc.edu>
2019-02-25 15:53:14 -0600

    all tests passed, slight modification

--- myswitch_lru.py
+++ myswitch_lru.py
@@ -5,8 +5,9 @@ import time
 def main(net):
     my_interfaces = net.interfaces()
     mymacs = [intf.ethaddr for intf in my_interfaces]
-    forwarding_table = {}
-    time_table = {}
+    #table is splited as 2 parts for simplcity
+    forwarding_table = {}#table that contains info about addr and port
+    time_table = {}#table that associate each addr with its time 
 
     while True:
         try:
@@ -23,16 +24,19 @@ def main(net):
                 lru_key = sorted_keys[0]
                 del time_table[lru_key]
                 del forwarding_table[lru_key]
-            time_table.update({header.src: time.time()})#keep a record of current header src addr & time
-        forwarding_table.update({header.src: input_port})#add mru entry
-        
+            time_table.update({header.src: time.time()})#add mru entry simultanously in 2 tables
+            forwarding_table.update({header.src: input_port})#add mru entry simultanously in 2 tables
+        else:
+            if(input_port != forwarding_table.get(header.src)):#incoming port info becomes different, update
+                forwarding_table[header.src] = input_port
+
         dest_port = forwarding_table.get(header.dst)
         if header.dst in mymacs:#dest is my switch
             log_debug("Packet intended for me")
-        elif dest_port is not None:
+        elif dest_port is not None:#entry exists
+            time_table.update({header.dst: time.time()})#update it to be the mru entry
             net.send_packet(dest_port, packet)
-            time_table.update({header.dst: time.time()})
-        else:#dest port not present in table, flood packet to all ports except incoming
+        else:#no entry for dest, flood packet to all ports except incoming
             for intf in my_interfaces:
                 if input_port != intf.name:
                     log_debug("Flooding packet {} to {}".format(packet, intf.name))
